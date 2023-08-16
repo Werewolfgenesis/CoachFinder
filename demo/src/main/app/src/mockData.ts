fd2ec85462928378a7602ef764c21309aef5b12a
rilcheva <reni.ilcheva@dxc.com>
2023-08-16 17:53:12 +0300

    Add mocked data for testing

---
+++ demo/src/main/app/src/mockData.ts
@@ -0,0 +1,33 @@
+
+import type {Coach} from '@/types/CoachType'
+
+export const mockedCoaches: Coach[] = [
+    {
+      id: 1,
+      firstName: 'Dimitar',
+      lastName: 'Sotirov',
+      areas: [{code: 'BE', desc: "Backend"}, {code: 'FE', desc: "Frontend"}],
+      rate: 60
+    },
+    {
+      id: 2,
+      firstName: 'Reni',
+      lastName: 'Ilcheva',
+      areas: [{code: 'FT', desc: "Fitness"}],
+      rate: 60
+    },
+    {
+      id: 3,
+      firstName: 'Maria',
+      lastName: 'Ivanova',
+      areas: [{code: 'BE', desc: "Backend"}],
+      rate: 40
+    },
+    {
+      id: 4,
+      firstName: 'Gosho',
+      lastName: 'Bandita',
+      areas: [{code: 'PM', desc: "Project Manager"}],
+      rate: 30
+    }
+  ]
---
+++ demo/src/main/app/src/types/CoachType.ts
@@ -0,0 +1,12 @@
+type area = {
+    code: string,
+    desc: string
+}
+
+export type Coach = {
+    id: number,
+    firstName: string,
+    lastName: string,
+    areas: area[],
+    rate: number
+}
