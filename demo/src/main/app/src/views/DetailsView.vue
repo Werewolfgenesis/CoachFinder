c2f75704b2f932bf37dbffab776473d497e4cc4c
rilcheva <reni.ilcheva@dxc.com>
2023-08-16 18:00:03 +0300

    Add register page  and details page with mocked data

---
+++ demo/src/main/app/src/views/DetailsView.vue
@@ -0,0 +1,31 @@
+<template>
+   <div class="container">
+    <div class="row justify-content-center text-center">
+      <div class="col">
+        <h2>Name</h2>
+        <h4>30$</h4>
+      </div>
+    </div>
+    <div class="row mb-4">
+        <div class="col">
+            <h4>Interested? Reach out now!</h4>
+            <custom-button title="Contact" type="button"></custom-button>
+        </div>
+        <div class="col text-center">
+            <ul class="d-flex flex-row gap-2">
+                <div>examle</div>
+                <div>examle</div>
+                <div>examle</div>
+            </ul>
+        </div>
+    </div>
+    <div class="row text-center">
+        <p>Description</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import CustomButton from '@/components/customComponents/CustomButton.vue';
+import type { Coach } from '@/types/CoachType';
+
+</script>
---
+++ demo/src/main/app/src/views/RegisterView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="text-center m-4"> 
+        <h2>REGISTER A COACH, NOW!</h2>
+    </div>
+    <form>
+      <div class="row">
+        <div class="mx-auto col-10 col-md-8 col-lg-6">
+          <div class="mb-3">
+            <label for="firstName" class="form-label">First Name</label>
+            <input v-model="firstName" type="text" class="form-control" id="firstName" aria-describedby="emailHelp" />
+          </div>
+          <div class="mb-3">
+            <label for="lastName" class="form-label">Last Name</label>
+            <input v-model="lastName" type="text" class="form-control" id="lastName" aria-describedby="emailHelp" />
+          </div>
+          <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea v-model="description"
+              class="form-control rounded-0"
+              id="exampleFormControlTextarea2"
+              rows="3"
+            ></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="hourlyRate" class="form-label">Hourly Rate</label>
+            <input
+              v-model="hourlyRate"
+              type="number"
+              class="form-control"
+              id="hourlyRate"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div v-for="(area, indx) in areas" :key="indx">
+            <div class="form-check m-2">
+              <input
+                v-model="skillsSelected"
+                class="form-check-input"
+                type="checkbox"
+                :value="area"
+                id="flexCheckDefault"
+              />
+              <label class="form-check-label" for="flexCheckDefault"> {{ area }} </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-center mb-4"> 
+          <custom-button @click="handleSubmit" title="Submit" type="submit" class="btn btn-md btn-primary"></custom-button>
+      </div>
+    </form>
+  </div>
+</template>
+<script setup lang="ts">
+import CustomButton from '@/components/customComponents/CustomButton.vue';
+
+import { ref } from 'vue'
+
+const areas = ref<string[]>(['Frontend Developer', 'Backend Developer', 'Career Advisory'])
+const skillsSelected = ref<string[]>([])
+
+const handleSubmit = () => {
+    
+}
+</script>
