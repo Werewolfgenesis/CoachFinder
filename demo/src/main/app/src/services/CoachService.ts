08d1f426696cde7c9cabc00142e84a3a837ee6cb
rilcheva <reni.ilcheva@dxc.com>
2023-08-17 16:42:27 +0300

    Fix url configs

--- demo/src/main/app/src/services/CoachService.ts
+++ demo/src/main/app/src/services/CoachService.ts
@@ -4,7 +4,8 @@ const BASE_URL = import.meta.env.BASE_URL
 
 export const getAllCoaches =async () => {
     try {
-        const response = await axios.get('http://localhost:8091/private/api/coaches/all')
+        
+        const response = await axios.get('/private/api/coaches/all')
         return response.data;
     } catch (err: any) {
        console.log('Error:', err);
