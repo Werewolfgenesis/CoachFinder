b767bfaa414bc5593b08607ed9ccfb8588554f4d
rilcheva <reni.ilcheva@dxc.com>
2023-10-06 10:58:52 +0300

    Add register form with simple validation

--- demo/src/main/app_ang/coach-finder/src/app/components/register/register.component.ts
+++ demo/src/main/app_ang/coach-finder/src/app/components/register/register.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Form,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { Area } from 'src/app/model/Area';
+import { AreasService } from 'src/app/services/areas.service';
+import { CoachService } from 'src/app/services/coach.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  allAreas: Area[] = [];
+  // selectedCheckboxes: Set<Area> = new Set();
+  selectedCheckboxes: string[] = [];
 
+  constructor(
+    private readonly areasService: AreasService,
+    private formBuilder: FormBuilder,
+    private readonly coachService: CoachService,
+    private router: Router
+  ) {}
+
+  profileForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    description: [''],
+    areas: [this.allAreas.forEach(area => area.code), Validators.required],
+    rate: ['', [Validators.min(0)]],
+    password: ['', Validators.required],
+  });
+
+  
+
+  ngOnInit() {
+    this.fetchAreas();
+  }
+
+
+  fetchAreas() {
+    this.areasService.getAllAreas().subscribe((response) => {
+      if (response) {
+        this.allAreas = response;
+      }
+    });
+  }
+
+  onCheckboxChange(selected: string, isChecked: boolean) {
+    if (isChecked) {
+      this.selectedCheckboxes.push(selected);
+    } else {
+      this.selectedCheckboxes.pop();
+    }
+    console.log(this.selectedCheckboxes);
+  }
+
+  submitForm() {
+    const coach: Object = {
+      ...this.profileForm.value,
+      areas: this.selectedCheckboxes,
+    };
+ 
+    
+    this.coachService.registerCoach(coach).subscribe((res) => {
+      this.router.navigate(['/all-coaches']);
+    });
+    
+    console.log(this.profileForm.invalid);
+  }
 }
--- demo/src/main/app_ang/coach-finder/src/app/services/coach.service.ts
+++ demo/src/main/app_ang/coach-finder/src/app/services/coach.service.ts
@@ -3,6 +3,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Coach } from '../model/Coach';
 
+// export type RegisterObject = {
+//   firstName: string,
+//   lastName: string,
+//   rate: number,
+//   description: string,
+//   areas: string[],
+//   password: string
+// }
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,7 +23,7 @@ export class CoachService {
     return this.httpClient.get<Coach[]>(this.BASE_URL + '/all');
   }
 
-  registerCoach(coach: Coach) {
+  registerCoach(coach: Object) {
     return this.httpClient.post<Coach>(this.BASE_URL, coach);
   }
 
